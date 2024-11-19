1c0a519990884b741d75392ed1ef893953fe1d78
a223205008 <jr.villagalvez@gmail.com>
2024-11-19 15:20:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("PutitaS ricotas")
+st.title("¿Cuántas proteínas contiene y cuáles son?")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
