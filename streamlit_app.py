c95a8f2ff00958d60cf299ddc5b8726e61e55aa1
a223205008 <jr.villagalvez@gmail.com>
2024-11-21 14:46:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+from collections import Counter
 
 st.title("¿Cuántas proteínas contiene y cuáles son?")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-$ pip install r- matplotlib.txt
-$ streamlit run streamlit_app.py
-import matplotlib.pyplot as plt
-from collections import Counter
 
 # Diccionario para la traducción del código genético
 codon_to_amino_acid = {
