d6f12eb4785b84c51922856166103803778d72a9
a223205008 <jr.villagalvez@gmail.com>
2024-11-23 20:38:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 from Bio.Seq import Seq
 from Bio.SeqUtils import molecular_weight
-from collections import Counter
-
-st.title("Diferencia proteica entre dos genes")
-
-# Diccionario de codones de ARN a aminoácidos
-codon_to_amino_acid = {
-    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L', 'CUU': 'L', 'CUC': 'L',
-    'CUA': 'L', 'CUG': 'L', 'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
-    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V', 'UCU': 'S', 'UCC': 'S',
-    'UCA': 'S', 'UCG': 'S', 'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
-    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T', 'GCU': 'A', 'GCC': 'A',
-    'GCA': 'A', 'GCG': 'A', 'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
-    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q', 'AAU': 'N', 'AAC': 'N',
-    'AAA': 'K', 'AAG': 'K', 'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
-    'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W', 'CGU': 'R', 'CGC': 'R',
-    'CGA': 'R', 'CGG': 'R', 'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
-    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
-}
 
 def traducir_adn(entrada_adn):
     """
@@ -47,7 +27,7 @@ def comparar_proteinas(proteina1, proteina2):
     diferencias = len(proteina1) - coincidencias
     return coincidencias, diferencias
 
-# Interfaz de usuario en Streamlit
+# Título de la app
 st.title('Comparador de Secuencias de ADN')
 
 # Introducción de las secuencias de ADN
