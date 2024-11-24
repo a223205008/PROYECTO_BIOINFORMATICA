233b779d947bfa9b57c6b2802fdbdb5488990d8d
a223205008 <jr.villagalvez@gmail.com>
2024-11-23 22:34:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,19 @@ def traducir_adn_a_proteina(secuencia_adn):
             proteina.append(traducir_codon(codon))
     return ''.join(proteina)
 
+def traducir_adn(entrada_adn):
+    """
+    Función para traducir una secuencia de ADN a una secuencia de proteína.
+    Utiliza el marco de lectura estándar (sin considerar mutaciones o marcos alternativos).
+    """
+    secuencia_adn = Seq(entrada_adn)
+    try:
+        # Traducción de la secuencia ADN a proteína
+        secuencia_proteina = secuencia_adn.translate()
+        return secuencia_proteina
+    except:
+        return None
+
 # Función para contar los aminoácidos presentes en la proteína
 def contar_aminoacidos(proteina):
     """
@@ -163,6 +176,11 @@ seq2 = st.text_area('Introduce la segunda secuencia de ADN', height=200)
 # Botón para realizar la comparación
 if st.button('Comparar Secuencias'):
     
+    if seq1 and seq2:
+    # Traducción de las secuencias de ADN a proteínas
+    proteina1 = traducir_adn(seq1)
+    proteina2 = traducir_adn(seq2)
+        
         if proteina1 and proteina2:
             # Mostrar las proteínas traducidas
             st.write(f'Proteína 1: {proteina1}')
